50b44e4456933769fc0fd39f6b8a366ed45fedc3
dev-shahed <shahedthedev@gmail.com>
2024-04-18 23:29:28 +0600

    Update Spring Security Configuration
    Changes include:
    - Disabling CSRF protection
    - Disabling CORS configuration
    - Allowing unauthenticated access to "/api/**"
    - Requiring authentication for any other request
    
    (

--- smsystem-backend/src/main/java/com/smsytem/students/config/SpringSecurityConfig.java
+++ smsystem-backend/src/main/java/com/smsytem/students/config/SpringSecurityConfig.java
@@ -3,11 +3,13 @@ package com.smsytem.students.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig {
     // private UserDetailsService userDetailsService;
@@ -16,34 +18,10 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeRequests(authorize -> {
-                    // >>>>>>>>Role base Security.....>>>>>>>
-                    // authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
-                    // authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
-                    // authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
-                    // authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN",
-                    // "USER");
-                    // authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN",
-                    // "USER");
-                    
-                    
-                    // authorize.requestMatchers(HttpMethod.POST, "/api/todos/**").hasRole("ADMIN");
-                    // authorize.requestMatchers("/api/auth/**").permitAll();
-                    authorize.anyRequest().authenticated();
-                }).httpBasic();
+        http.cors(cors -> cors.disable()).csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/**").permitAll()
+                        .anyRequest()
+                        .authenticated());
         return http.build();
     }
-
-    // @Bean
-    // public UserDetailsService userDetailsService() {
-    // PasswordEncoder encoder =
-    // PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    // UserDetails jhon =
-    // User.builder().username("jhon").password(encoder.encode("1234")).roles("USER").build();
-    // UserDetails admin =
-    // User.builder().username("admin").password(encoder.encode("password")).roles("ADMIN")
-    // .build();
-    // return new InMemoryUserDetailsManager(jhon, admin);
-    // }
 }
